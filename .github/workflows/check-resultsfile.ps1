a1ebf21ce4fee520c3d12a283b438caa00c003c2
Rob Bos <rajbos@users.noreply.github.com>
2023-05-30 11:19:35 +0000

    Add check for 3rd error

--- .github/workflows/check-resultsfile.ps1
+++ .github/workflows/check-resultsfile.ps1
@@ -7,31 +7,31 @@ $items = @(ConvertFrom-Json $result -Depth 10)
       
 $found1 = $false
 $found2 = $false
+$found3 = $false
 foreach ($item in $items) { 
     Write-Host "Found an error: "
     Write-Host "Message:" $item.message
     Write-Host "Filepath:" $item.filepath
     Write-Host "Line:" $item.line
 
-    if ($item.filepath -eq '.github/workflows/workflow-with-errors.yml' -and $item.line -eq 12) {
+    if ($item.filepath -eq '.github/workflows/workflow-with-errors.yml' -and $item.line -eq 6) {
         Write-Host "Found first file path and line number"
-        if ($item.message -eq "property ""test-local-action"" is not defined in object type {}") { 
+        if ($item.message -eq "job ""job-with-errors"" needs job ""test-local-action"" which does not exist in this workflow") { 
             $found1 = $true
-            Write-Host "found nr 1"
         }
-        else {
-            Write-Host "Expected message not found"
-            Write-Host "got      [$($item.message)]"
-            Write-Host "Expected [$('property ""test-local-action"" is not defined in object type {}')]"
-            Write-Host "Expected [$('property \"test-local-action\" is not defined in object type {}')]"
+    }
+
+    if ($item.filepath -eq '.github/workflows/workflow-with-errors.yml' -and $item.line -eq 12) {
+        Write-Host "Found second file path and line number"
+        if ($item.message -eq "property ""test-local-action"" is not defined in object type {}") { 
+            $found2 = $true
         }
     }
 
     if ($item.filepath -eq '.github/workflows/workflow-with-errors.yml' -and $item.line -eq 18) {
-        Write-Host "Found second file path and line number"
+        Write-Host "Found third file path and line number"
         if ($item.message -eq "property ""test-job"" is not defined in object type {}"){
-            $found2 = $true
-            Write-Host "found nr 2"
+            $found3 = $true
         }
     }
         
@@ -52,6 +52,13 @@ else {
     Write-Host "Found the second expected error succesfully"
 }
 
-if (!$found1 -or !$found2){
+if (!$found3){
+    Write-Host "Error finding the third expected result. Exiting with failure"
+} 
+else {
+    Write-Host "Found the third expected error succesfully"
+}
+
+if (!$found1 -or !$found2 -or !$found3){
     exit 1
 }
