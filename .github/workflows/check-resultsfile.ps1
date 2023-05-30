98d82475d55bac4d23587f2880d43d02cc7b2459
Rob Bos <rajbos@users.noreply.github.com>
2023-05-30 11:06:36 +0000

    hmm

--- .github/workflows/check-resultsfile.ps1
+++ .github/workflows/check-resultsfile.ps1
@@ -8,34 +8,34 @@ $items = @(ConvertFrom-Json $result -Depth 10)
 $found1 = $false
 $found2 = $false
 foreach ($item in $items) { 
-Write-Host "Found an error: "
-Write-Host "Message:" $item.message
-Write-Host "Filepath:" $item.filepath
-Write-Host "Line:" $item.line
+    Write-Host "Found an error: "
+    Write-Host "Message:" $item.message
+    Write-Host "Filepath:" $item.filepath
+    Write-Host "Line:" $item.line
 
-if ($item.filepath -eq '.github/workflows/workflow-with-errors.yml' -and $item.line -eq 12) {
-    Write-Host "Found first file path and line number"
-    if ($item.message -eq 'property \"test-local-action\" is not defined in object type {}') { 
-        $found1 = $true
-        Write-Host "found nr 1"
+    if ($item.filepath -eq '.github/workflows/workflow-with-errors.yml' -and $item.line -eq 12) {
+        Write-Host "Found first file path and line number"
+        if ($item.message -eq 'property \"test-local-action\" is not defined in object type {}') { 
+            $found1 = $true
+            Write-Host "found nr 1"
+        }
+        else {
+            Write-Host "Expected message not found"
+            Write-Host "got      [$($item.message)]"
+            Write-Host "Expected [$('property ""test-local-action"" is not defined in object type {}')]"
+            Write-Host "Expected [$('property \"test-local-action\" is not defined in object type {}')]"
+        }
     }
-    else {
-        Write-Host "Expected message not found"
-        Write-Host "got      [$($item.message)]"
-        Write-Host "Expected [$('property ""test-local-action"" is not defined in object type {}')]"
-        Write-Host "Expected [$('property \"test-local-action\" is not defined in object type {}')]"
-    }
-}
 
-if ($item.filepath -eq '.github/workflows/workflow-with-errors.yml' -and $item.line -eq 18) {
-    Write-Host "Found second file path and line number"
-    if ($item.message -eq 'property ""test-job"" is not defined in object type {}'){
-        $found2 = $true
-        Write-Host "found nr 2"
+    if ($item.filepath -eq '.github/workflows/workflow-with-errors.yml' -and $item.line -eq 18) {
+        Write-Host "Found second file path and line number"
+        if ($item.message -eq "property ""test-job"" is not defined in object type {}"){
+            $found2 = $true
+            Write-Host "found nr 2"
+        }
     }
-}
-    
-Write-Host "----------------------------------------------------------------------------------------------"   
+        
+    Write-Host "----------------------------------------------------------------------------------------------"   
 }
 
 if (!$found1){
